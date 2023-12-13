a81a7b0ce7dd28cd5196ed97baa77421f022dd96
MaximumQuiet <contact@maximumquiet.cyou>
2023-12-14 01:43:21 +0400

    Set preload to none

--- layouts/greetings-layout.js
+++ layouts/greetings-layout.js
@@ -5,7 +5,7 @@ import {memo} from "react";
 const GreetingsLayout = memo(({children}) => {
   return (
       <div className={"flex min-h-screen items-center"}>
-        <video muted autoPlay loop playsInline disablePictureInPicture preload="auto" className={"absolute h-full w-full object-cover"}>
+        <video muted autoPlay loop playsInline disablePictureInPicture preload="none" className={"absolute h-full w-full object-cover"}>
           <source src="/greetings.webm" type="video/webm" />
           <source src="/greetings.mp4" type="video/mp4" />
         </video>
--- layouts/rickroll-layout.js
+++ layouts/rickroll-layout.js
@@ -5,7 +5,7 @@ import {memo} from "react";
 const RickrollLayout = memo(({children}) => {
   return (
       <div className={"flex min-h-screen items-center"}>
-        <video muted autoPlay loop playsInline disablePictureInPicture preload="auto" className={"absolute h-full w-full object-cover"}>
+        <video muted autoPlay loop playsInline disablePictureInPicture preload="none" className={"absolute h-full w-full object-cover"}>
           <source src="/rickroll.webm" type="video/webm" />
           <source src="/rickroll.mp4" type="video/mp4" />
         </video>
