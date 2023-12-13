ab95f16158620209ddf08f99be488efbc360fe10
MaximumQuiet <contact@maximumquiet.cyou>
2023-12-14 01:38:24 +0400

    Add one more source for video element

--- layouts/greetings-layout.js
+++ layouts/greetings-layout.js
@@ -6,6 +6,7 @@ const GreetingsLayout = memo(({children}) => {
   return (
       <div className={"flex min-h-screen items-center"}>
         <video muted autoPlay loop playsInline disablePictureInPicture preload="auto" className={"absolute h-full w-full object-cover"}>
+          <source src="/greetings.mp4" type="video/mp4" />
           <source src="/greetings.webm" type="video/webm" />
         </video>
         {children}
