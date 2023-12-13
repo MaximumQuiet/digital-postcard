8ff3539101d2ebb205b5ceabf50a221ef75b73a0
MaximumQuiet <contact@maximumquiet.cyou>
2023-12-13 23:58:28 +0400

    Do not preload videos

--- layouts/greetings-layout.js
+++ layouts/greetings-layout.js
@@ -3,7 +3,7 @@
 const GreetingsLayout = ({children}) => {
   return (
       <div className={"flex min-h-screen items-center"}>
-        <video muted autoPlay loop playsInline disablePictureInPicture className={"absolute h-full w-full object-cover"}>
+        <video muted autoPlay loop playsInline disablePictureInPicture preload="none" className={"absolute h-full w-full object-cover"}>
           <source src="/greetings.webm" type="video/webm" />
         </video>
         {children}
