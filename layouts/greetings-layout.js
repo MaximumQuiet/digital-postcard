73b6389cb1d20f3b33fba42662c9139201282406
MaximumQuiet <contact@maximumquiet.cyou>
2023-12-14 01:41:33 +0400

    Change sources order

--- layouts/greetings-layout.js
+++ layouts/greetings-layout.js
@@ -6,8 +6,8 @@ const GreetingsLayout = memo(({children}) => {
   return (
       <div className={"flex min-h-screen items-center"}>
         <video muted autoPlay loop playsInline disablePictureInPicture preload="auto" className={"absolute h-full w-full object-cover"}>
-          <source src="/greetings.mp4" type="video/mp4" />
           <source src="/greetings.webm" type="video/webm" />
+          <source src="/greetings.mp4" type="video/mp4" />
         </video>
         {children}
       </div>
--- layouts/rickroll-layout.js
+++ layouts/rickroll-layout.js
@@ -6,8 +6,8 @@ const RickrollLayout = memo(({children}) => {
   return (
       <div className={"flex min-h-screen items-center"}>
         <video muted autoPlay loop playsInline disablePictureInPicture preload="auto" className={"absolute h-full w-full object-cover"}>
-          <source src="/rickroll.mp4" type="video/mp4" />
           <source src="/rickroll.webm" type="video/webm" />
+          <source src="/rickroll.mp4" type="video/mp4" />
         </video>
         {children}
       </div>
